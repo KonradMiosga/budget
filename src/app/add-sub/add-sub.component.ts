7c8d3eb36441a3f4a5983238075b42cd9c74767a
Konrad Miosga <konrad.miosga@gmail.com>
2025-01-28 17:53:27 +0100

    so much cool stuff

--- src/app/add-sub/add-sub.component.ts
+++ src/app/add-sub/add-sub.component.ts
@@ -1,12 +1,146 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { config } from '../../globals';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'an-add-sub',
   standalone: false,
-  
+
   templateUrl: './add-sub.component.html',
   styleUrl: './add-sub.component.css'
 })
-export class AddSubComponent {
+export class AddSubComponent implements OnInit {
+  amount: string = '';
+  name: string = '';
+  description: string = '';
+  type: string = '';
+  category: string = '';
+  // isCategoryEnabled: boolean = false;
+  isAddButtonEnabled: boolean = false;
+  currentState: { stateType: string; stateIntend: string } = { stateType: '', stateIntend: '' };
 
+  setEnvironment(): void {
+    switch (this.currentState.stateType) {
+      case 'Einnahme':
+        this.type = 'Einnahme';
+        break;
+      case 'Ausgabe':
+        this.type = 'Ausgabe';
+        break;
+      default:
+        this.router.navigate(['/home'])
+    }
+  }
+
+  flushForm(): void {
+    this.amount = '';
+    this.name = '';
+    this.description = '';
+
+    this.type = '';
+    this.category = '';
+    // this.isCategoryEnabled = false;
+    this.isAddButtonEnabled = false;
+  }
+
+
+  // expenseCategories: string[] = [
+  //   'Wohnen', 'Transport', 'Lebensmittel', 'Versicherung', 'Freizeit',
+  //   'Sonstiges', 'Gesundheit', 'Bildung', 'Kredite/Schulden', 'Steuern',
+  //   'Haushaltsgeräte', 'Kinderbetreuung', 'Geschenke/Spenden', 'Tierhaltung'
+  // ];
+  //
+  // incomeCategories: string[] = [
+  //   'Gehalt', 'Zinsen', 'Nebenjob', 'Mieteinnahmen', 'Rente/Pension',
+  //   'Unterstützung', 'Bonuszahlungen', 'Gewinnbeteiligung', 'Investitionen',
+  //   'Verkauf von Eigentum', 'Stipendien/Bildungsförderung'
+  // ];
+
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  entry!: { name: string; amount: string; type: string; category: string; description: string };
+
+  ngOnInit(): void {
+    this.entry = history.state.entry || null;
+    console.log('Entry when entring add/edit: ', this.entry);
+    if (this.entry) {
+      this.currentState.stateType = this.entry.type;
+      if (this.entry.name) {
+        this.currentState.stateIntend = 'ändern';
+        this.name = this.entry.name;
+        this.amount = this.entry.amount;
+        this.type = this.entry.type;
+        this.description = this.entry.description;
+      } else {
+        this.currentState.stateIntend = 'erstellen';
+      }
+    }
+
+    this.setEnvironment();
+
+
+  }
+
+  // onTypeChange(): void {
+  //   this.isCategoryEnabled = !!this.type;
+  //   if (!this.isCategoryEnabled) {
+  //     this.category = '';
+  //   }
+  // }
+
+  validateAddButton(): void {
+    this.isAddButtonEnabled = !!this.name && parseFloat(this.amount) > 0 && !!this.type && !!localStorage.getItem('myToken')
+  }
+
+  addEntry(): void {
+    console.log("Clickin' da button");
+    var token = localStorage.getItem('myToken');
+    if (!token) return;
+    const entryToPost = {
+      token: token,
+      entry: {
+        name: this.name,
+        amount: parseFloat(this.amount),
+        type: this.type,
+        category: this.category,
+        description: this.description
+      }
+    }
+
+    this.http.post(`${config.apiUrl}/entries`, entryToPost).subscribe({
+      next: (response: any) => {
+        console.log('Successfully posted entry', response);
+        alert(`${this.name} added successfully`);
+        this.flushForm()
+      },
+      error: (err) => {
+        console.log('Posting entry failed', err);
+
+      }
+    })
+
+  }
+
+  getEntries(): void {
+    var token = localStorage.getItem('myToken');
+    if (!token) return;
+
+    console.log(token);
+
+    this.http.get(`${config.apiUrl}/entries?token=${token}`).subscribe({
+      next: (data) => {
+        console.log('Got the entries.', data)
+      },
+      error: (err) => {
+        console.log('Could not get entries', err);
+      }
+    })
+  }
+
+  navToHome(): void {
+    this.flushForm();
+    this.router.navigate(['/home']);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Chart } from 'chart.js/auto';
 import { config } from '../../globals';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'an-home',
@@ -12,67 +13,110 @@ import { config } from '../../globals';
 })
 
 export class HomeComponent implements OnInit {
-  @ViewChild('chartEinzahlung', { static: false }) chartEinzahlung!: ElementRef;
-  @ViewChild('chartAuszahlung', { static: false }) chartAuszahlung!: ElementRef;
-  dataInput: { name: string; amount: number; type: string }[] = [];
-  einzahlungData: { name: string; amount: number; type: string }[] = [];
-  auszahlungData: { name: string; amount: number; type: string }[] = [];
+  @ViewChild('chartEinzahlung', { static: false }) chartEinzahlungRef!: ElementRef;
+  @ViewChild('chartAuszahlung', { static: false }) chartAuszahlungRef!: ElementRef;
+  @ViewChild('chartRest', { static: false }) chartRestRef!: ElementRef;
+  chartEinzahlung!: Chart;
+  chartAuszahlung!: Chart;
+
+  dataInput: { name: string; amount: number; type: string; description: string }[] = [];
+  einzahlungData: { name: string; amount: number; type: string; description: string; isHovered: boolean }[] = [];
+  auszahlungData: { name: string; amount: number; type: string; description: string; isHovered: boolean }[] = [];
   einzahlungTotal: number = 0;
   auszahlungTotal: number = 0;
   summeTotal: number = 0;
   exampleData = [{
     name: "Bsp.-Gehalt",
     amount: 2000,
-    type: "Einnahme"
+    type: "Einnahme",
+    description: ""
   }, {
     name: "Bsp.-Nebenjob",
     amount: 400,
-    type: "Einnahme"
+    type: "Einnahme",
+    description: ""
   }, {
     name: "Bsp.-Taschengeld",
     amount: 50,
-    type: "Einnahme"
+    type: "Einnahme",
+    description: ""
   }, {
     name: "Bsp.-Miete",
     amount: 600,
-    type: "Ausgabe"
+    type: "Ausgabe",
+    description: ""
   }, {
     name: "Bsp.-Netflix",
     amount: 10,
-    type: "Ausgabe"
+    type: "Ausgabe",
+    description: ""
   }, {
     name: "Bsp.-Versicherung",
     amount: 50,
-    type: "Ausgabe"
+    type: "Ausgabe",
+    description: ""
   }, {
     name: "Bsp.-Auto",
     amount: 250,
-    type: "Ausgabe"
+    type: "Ausgabe",
+    description: ""
   }, {
     name: "Bsp.-Essen",
     amount: 300,
-    type: "Ausgabe"
+    type: "Ausgabe",
+    description: ""
   }];
+  hasToken: boolean = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
-    // var token = localStorage.getItem('myToken');
-    // if (!token) {
-    //   this.populateSite(this.exampleData)
-    // } else {
-    //   this.getData()
-    // }
+    var token = localStorage.getItem('myToken');
+    this.hasToken = !!token;
+
+  }
+
+  navToAddIncome(): void {
+    const entry = {
+      name: '',
+      amount: '',
+      type: 'Einnahme',
+      category: '',
+      description: '',
+    };
+    this.router.navigate(['/add'], { state: { entry } })
+  }
+
+  navToEdit(selectedEntry: { name: string; amount: number; type: string; description: string }): void {
+    const entry = {
+      name: selectedEntry.name,
+      amount: selectedEntry.amount,
+      type: selectedEntry.type,
+      // category: '',
+      description: selectedEntry.description,
+    };
+    this.router.navigate(['/add'], { state: { entry } })
+  }
+
+  navToAddExpense(): void {
+    const entry = {
+      name: '',
+      amount: '',
+      type: 'Ausgabe',
+      category: '',
+      description: '',
+    };
+    this.router.navigate(['/add'], { state: { entry } })
   }
 
   ngAfterViewInit(): void {
     this.getData()
+    // this.createCharts()
   }
 
-  //Ich habe nie etwas schöneres gesehen =)
-  populateSite(dataInput: { name: string; amount: number; type: string; }[]): void {
-    this.einzahlungData = dataInput.filter((entry) => entry.type === 'Einnahme');
-    this.auszahlungData = dataInput.filter((entry) => entry.type === 'Ausgabe');
+  populateSite(dataInput: { name: string; amount: number; type: string; description: string }[]): void {
+    this.einzahlungData = dataInput.filter((entry) => entry.type === 'Einnahme').map((entry) => ({ ...entry, isHovered: false }));
+    this.auszahlungData = dataInput.filter((entry) => entry.type === 'Ausgabe').map((entry) => ({ ...entry, isHovered: false }));
 
     this.einzahlungTotal = this.einzahlungData.reduce((sum, entry) => sum + entry.amount, 0);
     this.auszahlungTotal = this.auszahlungData.reduce((sum, entry) => sum + entry.amount, 0);
@@ -85,20 +129,55 @@ export class HomeComponent implements OnInit {
     if (!token) {
       this.dataInput = this.exampleData
       this.populateSite(this.dataInput)
-      this.createCharts()
+      // this.createCharts()
+      if (!this.chartEinzahlung || !this.chartAuszahlung) {
+        this.createCharts();
+      } else {
+        this.updateCharts();
+      }
     } else {
-      this.http.get<{ name: string, amount: number, type: string }[]>(`${config.apiUrl}/entries?token=${token}`).subscribe({
+      this.http.get<{ name: string, amount: number, type: string, description: string }[]>(`${config.apiUrl}/entries?token=${token}`).subscribe({
         next: (values) => {
           console.log("Data loading complete", values)
           this.dataInput = values;
           this.populateSite(this.dataInput)
-          this.createCharts()
+          // this.createCharts()
+          if (!this.chartEinzahlung || !this.chartAuszahlung) {
+            this.createCharts();
+          } else {
+            this.updateCharts();
+          }
         },
         error: (err) => {
           console.log('Could not get data:', err);
         }
       })
     }
+  }
+
+
+  deleteEntry(data: { name: string; amount: number; type: string; isHovered: boolean }): void {
+    var token = localStorage.getItem('myToken');
+    if (!token) return;
+    const tokenName = {
+      token: token,
+      name: data.name
+    };
+
+    // Use POST or PUT for a body payload
+    this.http.delete(`${config.apiUrl}/entries`, {
+      body: tokenName,
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    }).subscribe({
+      next: (message) => {
+        console.log('Entry deleted successfully:', message);
+        this.getData();
+        this.updateCharts();
+      },
+      error: (err) => {
+        console.error('Error deleting entry:', err);
+      }
+    });
   }
 
   barColors = [
@@ -113,14 +192,15 @@ export class HomeComponent implements OnInit {
   ];
 
   createCharts(): void {
+    console.log('trying to create charts');
+
     const einzahlLabels = this.einzahlungData.sort((a, b) => b.amount - a.amount).map(entry => entry.name);
     const auszahlLabels = this.auszahlungData.sort((a, b) => b.amount - a.amount).map(entry => entry.name);
     const einzahlValues = this.einzahlungData.sort((a, b) => b.amount - a.amount).map(entry => entry.amount);
     const auszahlValues = this.auszahlungData.sort((a, b) => b.amount - a.amount).map(entry => entry.amount);
 
-    console.log(einzahlLabels);
-
-    new Chart(this.chartEinzahlung.nativeElement, {
+    const ctx1 = this.chartEinzahlungRef.nativeElement.getContext('2d');
+    this.chartEinzahlung = new Chart(ctx1, {
       type: 'pie',
       data: {
         labels: einzahlLabels,
@@ -133,7 +213,8 @@ export class HomeComponent implements OnInit {
       }
     })
 
-    new Chart(this.chartAuszahlung.nativeElement, {
+    const ctx2 = this.chartAuszahlungRef.nativeElement.getContext('2d');
+    this.chartAuszahlung = new Chart(ctx2, {
       type: 'pie',
       data: {
         labels: auszahlLabels,
@@ -145,6 +226,32 @@ export class HomeComponent implements OnInit {
 
       }
     })
+
+    // new Chart(this.chartRest.nativeElement, {
+    //   type: "pie",
+    //   data: {
+    //     labels: ["Ausgaben", "Überschuss"],
+    //     datasets: [{
+    //       backgroundColor: ["red", "yellow"],
+    //       data: [this.summeTotal, this.auszahlungTotal],
+    //       hoverOffset: 10
+    //     }]
+    //   }
+    // });
+  }
+
+  updateCharts(): void {
+    if (this.chartEinzahlung) {
+
+      console.log('trying to update charts');
+      this.chartEinzahlung.data.labels = this.einzahlungData.map(entry => entry.name);
+      this.chartEinzahlung.data.datasets[0].data = this.einzahlungData.map(entry => entry.amount);
+      this.chartEinzahlung.update(); // Re-render the chart
+
+      this.chartAuszahlung.data.labels = this.auszahlungData.map(entry => entry.name);
+      this.chartAuszahlung.data.datasets[0].data = this.auszahlungData.map(entry => entry.amount);
+      this.chartAuszahlung.update(); // Re-render the chart
+    }
   }
 
 }
